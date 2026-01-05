0b3d032e47f1e5e4c992085b20c69fbc5ff98238
Divyanshu Sargara <divyanshbhati1227@gmail.com>
2026-01-05 22:34:10 +0530

    all works fine now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
+import React from "react";
 import { ChatProvider } from "./context/ChatContext";
 import SideBar from "./components/SideBar";
 import Center from "./components/Center";
-import Nav from "./components/Nav";
 
 function App() {
   return (
